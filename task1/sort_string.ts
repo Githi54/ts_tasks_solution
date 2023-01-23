3433b1585f4c9a0dd3e350f808b4c9d0e4bf0a5f
Mikita Havryliuk <chinter.games1@gmail.com>
2023-01-23 22:05:36 +0200

    solved task 1

---
+++ task1/sort_string.ts
@@ -0,0 +1,22 @@
+function sortString(str: string): string {
+    const arr = str.split(' ')
+    let position = 1;
+    let result = '';
+    let i = 0;
+  
+    while (position <= arr.length) {
+        if (i === arr.length) {
+            i = 0;
+        }
+  
+        if (arr[i].includes(`${position}`)) {
+            result += arr[i];
+            result += ' ';
+            position++;
+        }
+  
+        i++;
+    }
+       
+    return result;
+}
